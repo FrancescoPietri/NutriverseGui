9f1bdb724edc03a39d4c8e5f5567876eab7cf4bf
francesco <francesco.pietri@studenti.unitn.it>
2024-06-19 12:00:26 +0200

    confirmation email

--- NutriVerse-vue-testing/vue-ui/src/App.vue
+++ NutriVerse-vue-testing/vue-ui/src/App.vue
@@ -10,51 +10,10 @@ export default {
       IdCode: "ALDKSIFN",
       interactionCode: "",
       logged: false,
+      auth_token: "",
     }
   },
 
-  async function_query(method, uri, data) {
-    console.log("Querying " + "https://nutriverse.onrender.com/api/v1/" + uri)
-    const headers = {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    }
-    const response = await fetch("https://nutriverse.onrender.com/api/v1/" + uri, {
-      method: method,
-      headers: headers,
-      body: JSON.stringify(data),
-      cache: 'no-cache',
-    })
-    return await response.json()
-  },
-
-  async function_verify(token) {
-    return await this.function_query("PUT", "user", { token })
-  },
-
-  created() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
-    alert(token)
-    if (token) {
-      this.authToken = token;
-      localStorage.setItem('authToken', token);
-      let expires = new Date();
-      expires.setTime(expires.getTime() + 3600 * 1000);
-      document.cookie = `auth_token=${token}; expires=${expires.toUTCString()}; path=/`;
-      this.function_verify(token);
-      this.logged=true;
-    } else {
-      // Carica il token dal localStorage se esiste
-      this.authToken = localStorage.getItem('authToken') || "";
-    }
-
-    // Recupera i dati dal localStorage quando l'app viene creata
-    this.interacting = JSON.parse(localStorage.getItem('interacting')) || false;
-    this.IdCode = localStorage.getItem('IdCode') || "ALDKSIFN";
-    this.interactionCode = localStorage.getItem('interactionCode') || "";
-    this.logged = JSON.parse(localStorage.getItem('logged')) || false;
-  },
   watch: {
     interacting(newVal) {
       localStorage.setItem('interacting', JSON.stringify(newVal));
@@ -69,7 +28,29 @@ export default {
       localStorage.setItem('logged', JSON.stringify(newVal));
     },
   },
+
   methods: {
+
+    async function_query(method, uri, data) {
+      console.log("Querying " + "https://nutriverse.onrender.com/api/v1/" + uri)
+      const headers = {
+        'auth-token': this.auth_token,
+        'Contefunction_verifynt-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+      const response = await fetch("https://nutriverse.onrender.com/api/v1/" + uri, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(data),
+        cache: 'no-cache',
+      })
+      return await response.json()
+    },
+
+    async function_verify(confirmationToken) {
+      return await this.function_query("PUT", `user/${confirmationToken}`)
+    },
+
     login() {
       this.logged = true;
     },
@@ -85,6 +66,26 @@ export default {
       this.interacting = false;
     }
   },
+
+  created() {
+    // Recupera i dati dal localStorage quando l'app viene creata
+    this.interacting = JSON.parse(localStorage.getItem('interacting')) || false;
+    this.IdCode = localStorage.getItem('IdCode') || "ALDKSIFN";
+    this.interactionCode = localStorage.getItem('interactionCode') || "";
+    this.logged = JSON.parse(localStorage.getItem('logged')) || false;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+      this.auth_token = token;
+      let expires = new Date();
+      expires.setTime(expires.getTime() + 3600 * 1000);
+      document.cookie = `auth_token=${token}; expires=${expires.toUTCString()}; path=/`;
+      this.function_verify(token);
+      this.logged=true;
+    }
+  },
+
   components: {
     InteractionDashboard,
     MainDashboard,
